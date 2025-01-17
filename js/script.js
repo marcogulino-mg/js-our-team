10b749b01049d5a330669d736160a53b1956f484
marco-gulino <marcogulino98@gmail.com>
2025-01-17 18:34:03 +0100

    [feat/js] create function that prepares "teamMembers" properties for insertion into HTML

--- js/script.js
+++ js/script.js
@@ -1,43 +1,66 @@
+//Functions
+
+//Funzione che prepara i membri del team
+//da aggiungere all'inserimento nell'HTML
+function createTeamMembers(object) {
+  const { name, role, img, email } = object;
+
+  let html = `
+    <div class="card">
+              <img src="${object.img}" alt="${teamMembers.name}" />
+              <div class="description">
+                <h3>${object.name}</h3>
+                <span class="occupation">${object.role}</span>
+                <a href="" class="email">${object.email}</a>
+              </div>
+            </div>
+   `;
+
+  return html;
+}
+
+//Lista dei membri del team
 const teamMembers = [
   {
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png",
+    img: "./img/male1.png",
   },
 
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png",
+    img: "./img/female1.png",
   },
 
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png",
+    img: "./img/male2.png",
   },
 
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png",
+    img: "./img/female2.png",
   },
 
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png",
+    img: "./img/male3.png",
   },
 
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png",
+    img: "./img/female3.png",
   },
 ];
+
